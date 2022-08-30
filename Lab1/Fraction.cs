c0f6ae40b00a0fa4b3846769ccd66dec7d854d1a
nickhildebrant <nickhildebrant09@gmail.com>
2022-08-30 16:55:40 -0700

    Added Fraction Class

--- Lab1/Fraction.cs
+++ Lab1/Fraction.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CPI311.Labs {
     internal class Fraction
     {
+        public int numerator;
+        public int denominator;
 
+        public int Numerator
+        {
+            get { return numerator; }
+            set { numerator = value; }
+        }
+
+        public int Denominator
+        {
+            get { return denominator; }
+            set { denominator = value; }
+        }
+
+        public Fraction(int n = 0, int d = 1)
+        {
+            numerator = n;
+            if (d == 0) d = 1; // avoid dividing by 0
+            denominator = d;
+        }
+
+        // Method
+        public static Fraction multiply(Fraction a, Fraction b)
+        {
+            return new Fraction(a.numerator + b.numerator, a.denominator + b.denominator);
+        }
     }
 }
