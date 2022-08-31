0f84627bf629c3cc36a3e70175074521898d7b0f
nickhildebrant <nickhildebrant09@gmail.com>
2022-08-30 18:43:08 -0700

    Sprite Class

--- GameEngine/Sprite.cs
+++ GameEngine/Sprite.cs
@@ -13,7 +13,15 @@ namespace CPI311.GameEngine
         // Constructor
         public Sprite(Texture2D texture)
         {
-
+            Texture = texture;
+            Position = new Vector2(0, 0);
+            Source = new Rectangle(0, 0, texture.Width, texture.Height);
+            Color = Color.White;
+            Rotation = 0;
+            Origin = new Vector2(Texture.Width / 2, Texture.Height / 2);
+            Scale = new Vector2(1, 1);
+            Effect = SpriteEffects.None;
+            Layer = 1;
         }
 
         // Properties
@@ -24,7 +32,14 @@ namespace CPI311.GameEngine
         public Single Rotation { get; set; }
         public Vector2 Origin { get; set; }
         public Vector2 Scale { get; set; }
-        public SpriteEffect Effect { get; set; }
+        public SpriteEffects Effect { get; set; }
         public Single Layer { get; set; }
+
+        // Update and Draw
+        public virtual void Update() { }
+        public virtual void Draw(SpriteBatch spriteBatch) 
+        {
+            spriteBatch.Draw(Texture, Position, Source, Color, Rotation, Origin, Scale, Effect, Layer);
+        }
     }
 }
