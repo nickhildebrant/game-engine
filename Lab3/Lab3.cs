b56d6444ca1a5e56375778b897521f2c288f28af
nickhildebrant <nickhildebrant09@gmail.com>
2022-09-11 16:37:50 -0700

    Edited Controls

--- Lab3/Lab3.cs
+++ Lab3/Lab3.cs
@@ -18,7 +18,9 @@ namespace CPI311.Labs
         Vector3 modelPosition = new Vector3(0, 0, 0);
 
         SpriteFont font;
-        string cameraString = "Perspective";
+        bool cameraMode = true;
+
+        bool orderToggle = true;
 
         // *** Matrix for 3D
         Matrix world;
@@ -69,30 +71,40 @@ namespace CPI311.Labs
             InputManager.Update();
             Time.Update(gameTime);
 
-            // Move camera with WASD in 3D world
-            if (InputManager.IsKeyDown(Keys.W)) cameraPosition += 5 * Vector3.Up * Time.ElapsedGameTime;
-            if (InputManager.IsKeyDown(Keys.A)) cameraPosition += 5 * Vector3.Left * Time.ElapsedGameTime;
-            if (InputManager.IsKeyDown(Keys.S)) cameraPosition += 5 * Vector3.Down * Time.ElapsedGameTime;
-            if (InputManager.IsKeyDown(Keys.D)) cameraPosition += 5 * Vector3.Right * Time.ElapsedGameTime;
-
-            // Move model in 3D world with the arrow keys
-            if (InputManager.IsKeyDown(Keys.Up)) modelPosition += 5 * Vector3.Up * Time.ElapsedGameTime;
-            if (InputManager.IsKeyDown(Keys.Down)) modelPosition += 5 * Vector3.Down * Time.ElapsedGameTime;
-            if (InputManager.IsKeyDown(Keys.Left)) modelPosition += 5 * Vector3.Left * Time.ElapsedGameTime;
-            if (InputManager.IsKeyDown(Keys.Right)) modelPosition += 5 * Vector3.Right * Time.ElapsedGameTime;
-
-            // Change scale of the model with Shift+UP/DOWN
-            if (InputManager.IsKeyDown(Keys.Up) && InputManager.IsKeyDown(Keys.LeftShift)) modelScale += 5 * Time.ElapsedGameTime;
-            if (InputManager.IsKeyDown(Keys.Down) && InputManager.IsKeyDown(Keys.LeftShift)) modelScale -= 5 * Time.ElapsedGameTime;
-
-            // Tab changes the camera mode
-            if (InputManager.IsKeyPressed(Keys.Tab))
+            // If the shift key is held down
+            if(InputManager.IsKeyDown(Keys.LeftShift))
             {
-                if (cameraString == "Perspective")  { projection = Matrix.CreateOrthographic(MathHelper.PiOver2, 1.33f, 0.01f, 1000f); cameraString = "Orthographic"; }
-                if (cameraString == "Orthographic") { projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver2, 1.33f, 0.01f, 1000f); cameraString = "Perspective"; }
+                // Change scale of the model with Shift+UP/DOWN
+                if (InputManager.IsKeyDown(Keys.Up)) modelScale += 5 * Time.ElapsedGameTime;
+                if (InputManager.IsKeyDown(Keys.Down)) modelScale -= 5 * Time.ElapsedGameTime;
+            }
+            else // The shift key is not being held down
+            {
+                // Move camera with WASD in 3D world
+                if (InputManager.IsKeyDown(Keys.W)) cameraPosition += 5 * Vector3.Up * Time.ElapsedGameTime;
+                if (InputManager.IsKeyDown(Keys.A)) cameraPosition += 5 * Vector3.Left * Time.ElapsedGameTime;
+                if (InputManager.IsKeyDown(Keys.S)) cameraPosition += 5 * Vector3.Down * Time.ElapsedGameTime;
+                if (InputManager.IsKeyDown(Keys.D)) cameraPosition += 5 * Vector3.Right * Time.ElapsedGameTime;
+
+                // Move model in 3D world with the arrow keys
+                if (InputManager.IsKeyDown(Keys.Up)) modelPosition += 5 * Vector3.Up * Time.ElapsedGameTime;
+                if (InputManager.IsKeyDown(Keys.Down)) modelPosition += 5 * Vector3.Down * Time.ElapsedGameTime;
+                if (InputManager.IsKeyDown(Keys.Left)) modelPosition += 5 * Vector3.Left * Time.ElapsedGameTime;
+                if (InputManager.IsKeyDown(Keys.Right)) modelPosition += 5 * Vector3.Right * Time.ElapsedGameTime;
             }
 
-            world = Matrix.CreateScale(modelScale) * Matrix.CreateFromYawPitchRoll(0, 0, 0) * Matrix.CreateTranslation(modelPosition);
+            // Tab changes the camera mode
+            if (InputManager.IsKeyPressed(Keys.Tab)) cameraMode = !cameraMode;
+
+            // Space key changes the order in which the world is created
+            if (InputManager.IsKeyPressed(Keys.Space)) orderToggle = !orderToggle;
+
+            if (cameraMode) projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver2, 1.33f, 0.01f, 1000f);
+            else projection = Matrix.CreateOrthographic(MathHelper.PiOver2, 1.33f, 0.01f, 1000f);
+
+            if(orderToggle) world = Matrix.CreateScale(modelScale) * Matrix.CreateFromYawPitchRoll(0, 0, 0) * Matrix.CreateTranslation(modelPosition); // scale * rot * trans
+            else world = Matrix.CreateTranslation(modelPosition) * Matrix.CreateFromYawPitchRoll(0, 0, 0) * Matrix.CreateScale(modelScale);            // trans * rot * scal
+
             view = Matrix.CreateLookAt(cameraPosition, new Vector3(0, 0, -1), new Vector3(0, 1, 0));
 
             base.Update(gameTime);
@@ -106,11 +118,21 @@ namespace CPI311.Labs
             model.Draw(world, view, projection);
 
             _spriteBatch.Begin();
-            _spriteBatch.DrawString(font, "Current Camera Mode: " + cameraString, new Vector2(5, 5), Color.Black);
-            _spriteBatch.DrawString(font, "Tab: Change Camera Mode", new Vector2(5, 20), Color.Black);
-            _spriteBatch.DrawString(font, "WASD Keys: Move Camera", new Vector2(5, 35), Color.Black);
 
-            _spriteBatch.DrawString(font, "Model Position: (" + modelPosition.X + ", " + modelPosition.Y + ")", new Vector2(5, 50), Color.Black);
+            // printing the method of multiplying matricies
+            if(orderToggle) _spriteBatch.DrawString(font, "SPACE: World = Scale * Rotation * Translation", new Vector2(5, 10), Color.Black);
+            else _spriteBatch.DrawString(font, "SPACE: World = Translation * Rotation * Scale", new Vector2(5, 10), Color.Black);
+
+            // printing camera mode
+            if(cameraMode) _spriteBatch.DrawString(font, "TAB: Current Camera Mode: Perspective", new Vector2(5, 30), Color.Black);
+            else _spriteBatch.DrawString(font, "TAB: Current Camera Mode: Orthographic", new Vector2(5, 30), Color.Black);
+
+            // Movement
+            _spriteBatch.DrawString(font, "WASD Keys: Move Camera", new Vector2(5, 50), Color.Black);
+
+            _spriteBatch.DrawString(font, "Arrow Keys: Move Model", new Vector2(5, 130), Color.Black);
+            _spriteBatch.DrawString(font, "Model Position: (" + modelPosition.X + ", " + modelPosition.Y + ")", new Vector2(5, 150), Color.Black);
+            _spriteBatch.DrawString(font, "Shift + Up/Down: Scale Model", new Vector2(5, 170), Color.Black);
 
             _spriteBatch.End();
 
