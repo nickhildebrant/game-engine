98916856b479573e84b817baec5088ebb8b7f82b
nickhildebrant <nickhildebrant09@gmail.com>
2022-10-04 18:19:04 -0700

    Renderer Class

---
+++ GameEngine/Renderer.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CPI311.GameEngine
+{
+    public class Renderer : Component, IRenderable
+    {
+        public Material Material { get; set; }
+        public Model ObjectModel;
+        public Transform ObjectTransform;
+        public int CurrentTechnique;
+        public GraphicsDevice g;
+
+      public Renderer(Model objModel, Transform objTransform, Camera camera, ContentManager content, GraphicsDevice graphicsDevice, ..., string filename, ...)
+      {
+            if (filename != null) Material = new Material(objTransfrom.World, ...);
+            else Material = null;
+            ... // set the other properties;
+}
+        public virtual void Draw()
+        {
+            if (Material != null)
+            {
+                Material.Camera = Camera; // Update Material's properties
+                
+                for (int i = 0; i < Material.Passes; i++)
+                {
+                    Material.Apply(i); // Look at the Material's Apply method
+                    foreach (ModelMesh mesh in objModel.Meshes)
+                        foreach (ModelMeshPart part in mesh.MeshParts)
+                        {
+                            g.SetVertexBuffer(...)
+                            g.Indices = ...
+                            g.DrawIndexedPrimitives(...);
+                        }
+                }
+            }
+            else objModel.Draw(Transform.World, Camera.View, Camera.Projection);
+        }
+    }
+}
