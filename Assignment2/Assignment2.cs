2559a78f9e55396b7d9492d7b9aeb6224023a612
nickhildebrant <nickhildebrant09@gmail.com>
2022-10-08 17:05:13 -0700

    Started Assignment2

---
+++ Assignment2/Assignment2.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using CPI311.GameEngine;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Assignment2
+{
+    public class Assignment2 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        SpriteFont font;
+
+        Camera camera;
+        Transform cameraTransform;
+
+        Light light;
+
+        Model plane;
+        Transform planeTransform;
+
+        Model sunModel, mercModel, earthModel, moonModel;
+
+        List<Renderer> renderers;
+
+        public Assignment2()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            InputManager.Initialize();
+            Time.Initialize();
+
+            renderers = new List<Renderer>();
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            font = Content.Load<SpriteFont>("font");
+
+            camera = new Camera();
+            cameraTransform = new Transform();
+            cameraTransform.LocalPosition = Vector3.Backward * 5;
+            camera.Transform = cameraTransform;
+
+            light = new Light();
+            Transform lightTransform = new Transform();
+            lightTransform.LocalPosition = Vector3.Backward * 10 + Vector3.Right * 5;
+            light.Transform = lightTransform;
+
+            plane = Content.Load<Model>("Plane");
+            planeTransform = new Transform();
+            renderers.Add(new Renderer(plane, planeTransform, camera, light, Content, GraphicsDevice, 20f, Content.Load<Texture2D>("Square"), "SimpleShading", 1));
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            InputManager.Update();
+            Time.Update(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            for (int i = 0; i < renderers.Count; i++) renderers[i].Draw();
+
+            base.Draw(gameTime);
+        }
+    }
+}
