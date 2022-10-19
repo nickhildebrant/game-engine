7aaf0eff0e05d938991f5aa07a571ffd2bf99ed4
Nick Hildebrant <nickhildebrant09@gmail.com>
2022-10-18 19:08:03 -0700

    Finished Lab8

--- Lab8/Lab8.cs
+++ Lab8/Lab8.cs
@@ -5,6 +5,7 @@ using Microsoft.Xna.Framework.Input;
 using System.Collections.Generic;
 
 using CPI311.GameEngine;
+using System;
 
 namespace CPI311.Labs
 {
@@ -18,6 +19,9 @@ namespace CPI311.Labs
         SoundEffectInstance soundInstance;
 
         Model model;
+        Transform modelTransform;
+        //Collider sphereCollider;
+        Texture texture;
         Camera camera, topDownCamera;
 
         Effect effect;
@@ -29,6 +33,7 @@ namespace CPI311.Labs
         public Lab8()
         {
             _graphics = new GraphicsDeviceManager(this);
+            _graphics.GraphicsProfile = GraphicsProfile.HiDef;
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
@@ -38,6 +43,10 @@ namespace CPI311.Labs
             Time.Initialize();
             InputManager.Initialize();
             ScreenManager.Initialize(_graphics);
+
+            transforms = new List<Transform>();
+            colliders = new List<Collider>();
+            cameras = new List<Camera>();
 
             base.Initialize();
         }
@@ -49,6 +58,37 @@ namespace CPI311.Labs
             ScreenManager.Setup(true, 1920, 1080);
 
             gunSound = Content.Load<SoundEffect>("Gun");
+
+            effect = Content.Load<Effect>("SimpleShading");
+
+            model = Content.Load<Model>("Sphere");
+            modelTransform = new Transform();
+            SphereCollider sphereCollider = new SphereCollider();
+            sphereCollider.Radius = 1.0f * modelTransform.LocalScale.Y;
+            sphereCollider.Transform = modelTransform;
+
+            transforms.Add(modelTransform);
+            colliders.Add(sphereCollider);
+
+            texture = Content.Load<Texture2D>("Square");
+
+            camera = new Camera();
+            camera.Transform = new Transform();
+            camera.Transform.LocalPosition = Vector3.Backward * 5;
+            camera.Position = new Vector2(0f, 0f);
+            camera.Size = new Vector2(0.5f, 1f);
+            camera.AspectRatio = camera.Viewport.AspectRatio;
+
+            topDownCamera = new Camera();
+            topDownCamera.Transform = new Transform();
+            topDownCamera.Transform.LocalPosition = Vector3.Up * 10;
+            topDownCamera.Transform.Rotate(Vector3.Right, -MathHelper.PiOver2);
+            topDownCamera.Position = new Vector2(0.5f, 0f);
+            topDownCamera.Size = new Vector2(0.5f, 1f);
+            topDownCamera.AspectRatio = topDownCamera.Viewport.AspectRatio;
+
+            cameras.Add(topDownCamera);
+            cameras.Add(camera);
         }
 
         protected override void Update(GameTime gameTime)
@@ -64,6 +104,22 @@ namespace CPI311.Labs
                 instance.Play();
             }
 
+            Ray ray = camera.ScreenPointToWorldRay(InputManager.GetMousePosition());
+
+            foreach (Collider collider in colliders)
+            {
+                if (collider.Intersects(ray) != null)
+                {
+                    effect.Parameters["DiffuseColor"].SetValue(Color.Red.ToVector3());
+                    (model.Meshes[0].Effects[0] as BasicEffect).DiffuseColor = Color.Blue.ToVector3();
+                }
+                else
+                {
+                    effect.Parameters["DiffuseColor"].SetValue(Color.Blue.ToVector3());
+                    (model.Meshes[0].Effects[0] as BasicEffect).DiffuseColor = Color.Red.ToVector3();
+                }
+            }
+
             base.Update(gameTime);
         }
 
@@ -71,7 +127,40 @@ namespace CPI311.Labs
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            foreach (Camera camera in cameras)
+            {
+                GraphicsDevice.DepthStencilState = new DepthStencilState();
+                GraphicsDevice.Viewport = camera.Viewport;
+                Matrix view = camera.View;
+                Matrix projection = camera.Projection;
+                effect.CurrentTechnique = effect.Techniques[1];
+                effect.Parameters["View"].SetValue(view);
+                effect.Parameters["Projection"].SetValue(projection);
+                effect.Parameters["LightPosition"].SetValue(Vector3.Backward * 10 + Vector3.Right * 5);
+                effect.Parameters["CameraPosition"].SetValue(camera.Transform.Position);
+                effect.Parameters["Shininess"].SetValue(2f);
+                effect.Parameters["AmbientColor"].SetValue(new Vector3(0.2f, 0.2f, 0.2f));
+                effect.Parameters["SpecularColor"].SetValue(new Vector3(0, 0, 0.5f));
+                effect.Parameters["DiffuseTexture"].SetValue(texture);
+
+                foreach (Transform transform in transforms)
+                {
+                    effect.Parameters["World"].SetValue(transform.World);
+                    foreach (EffectPass pass in effect.CurrentTechnique.Passes)
+                    {
+                        pass.Apply();
+                        foreach (ModelMesh mesh in model.Meshes)
+                        {
+                            foreach (ModelMeshPart part in mesh.MeshParts)
+                            {
+                                GraphicsDevice.SetVertexBuffer(part.VertexBuffer);
+                                GraphicsDevice.Indices = part.IndexBuffer;
+                                GraphicsDevice.DrawIndexedPrimitives(PrimitiveType.TriangleList, part.VertexOffset, part.StartIndex, part.PrimitiveCount);
+                            }
+                        }
+                    }
+                }
+            }
 
             base.Draw(gameTime);
         }
