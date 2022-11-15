6f9476c7af00559402a53c502100d9ad101202c4
nickhildebrant <nickhildebrant09@gmail.com>
2022-11-14 23:47:41 -0700

    Starting Assignement4

---
+++ Assignment4/Bullet.cs
@@ -0,0 +1,50 @@
+﻿using CPI311.GameEngine;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace Assignment4
+{
+    public class Bullet : GameObject
+    {
+        public bool isActive { get; set; }
+
+        public Bullet(ContentManager Content, Camera camera, GraphicsDevice graphicsDevice, Light light) : base()
+        {
+            // *** Add Rigidbody
+            Rigidbody rigidbody = new Rigidbody();
+            rigidbody.Transform = Transform;
+            rigidbody.Mass = 1;
+            Add<Rigidbody>(rigidbody);
+            // *** Add Renderer
+            Texture2D texture = Content.Load<Texture2D>("Square");
+            Renderer renderer = new Renderer(Content.Load<Model>("Models/bullet"), Transform, camera, light, Content, graphicsDevice, 20f, texture, null, 1);
+            Add<Renderer>(renderer);
+
+            // *** Add collider
+            SphereCollider sphereCollider = new SphereCollider();
+            sphereCollider.Radius = renderer.ObjectModel.Meshes[0].BoundingSphere.Radius;
+            sphereCollider.Transform = Transform;
+            Add<Collider>(sphereCollider);
+            //*** Additional Property (for Asteroid, isActive = true)
+            isActive = false;
+        }
+
+        public void Update()
+        {
+            if (!isActive) return;
+
+            if (Transform.Position.X > GameConstants.PlayfieldSizeX ||
+               Transform.Position.X < -GameConstants.PlayfieldSizeX ||
+               Transform.Position.Z > GameConstants.PlayfieldSizeY ||
+               Transform.Position.Z < -GameConstants.PlayfieldSizeY)
+            {
+                isActive = false;
+                Rigidbody.Velocity = Vector3.Zero; // stop moving
+            }
+
+            base.Update();
+        }
+    }
+}
---
+++ Assignment4/Ship.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace GoingBeyond4
+{
+    class Ship
+    {
+        public Model Model;
+        public Matrix[] Transforms;
+
+        //Position of the model in world space
+        public Vector3 Position = Vector3.Zero;
+
+        //Velocity of the model, applied each frame to the model's position
+        public Vector3 Velocity = Vector3.Zero;
+
+        public Matrix RotationMatrix = Matrix.Identity;
+        private float rotation = 0.0f;
+        public float Rotation
+        {
+            get { return rotation; }
+            set
+            {
+                float newVal = value;
+                while (newVal >= MathHelper.TwoPi)
+                {
+                    newVal -= MathHelper.TwoPi;
+                }
+                while (newVal < 0)
+                {
+                    newVal += MathHelper.TwoPi;
+                }
+
+                if (rotation != newVal)
+                {
+                    rotation = newVal;
+                    RotationMatrix = Matrix.CreateRotationY(rotation);
+                }
+
+            }
+        }
+
+        public void Update(GamePadState controllerState)
+        {
+            // Rotate the model using the left thumbstick, and scale it down.
+            Rotation -= controllerState.ThumbSticks.Left.X * 0.10f;
+
+            // Finally, add this vector to our velocity.
+            Velocity += RotationMatrix.Forward * 1.0f * controllerState.Triggers.Right;
+        }
+    }
+}
